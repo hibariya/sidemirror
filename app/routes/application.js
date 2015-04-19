340a8196eab4e8f67257980154490025fb3c4b03
Hika Hibariya <hibariya@gmail.com>
2015-04-19 15:39:52 +0900

    Use lower quality

--- app/routes/application.js
+++ app/routes/application.js
@@ -16,14 +16,15 @@ export default Ember.Route.extend({
 
   join: function() {
     // TODO: Firefox, Opera
-    navigator.webkitGetUserMedia({ audio: true, video: true }, (stream) => {
+    // TODO: make options configurable
+    navigator.webkitGetUserMedia({ audio: true, video: { mandatory: { maxWidth: 640, maxHeight: 360, maxFrameRate: 15 } } }, (stream) => {
       this.get('session.person').set('stream', stream);
 
       this.controllerFor('peer').subscribe();
 
       this.transitionTo('people');
     }, function(error) {
-      alert(error.name + ': ' + error.message);
+      console.log(error.name + ': ' + error.message);
     });
   }
 });
